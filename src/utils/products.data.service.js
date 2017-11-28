f3061c39b75cc1f0ec2a4b7a93524d8fdd7a6e6c
Josue Rembao <josue.garcia.rembao@gmail.com>
2017-11-28 07:37:41 -0700

    Eliminado for de product.data.service y eliminado editar productos del menu lateral

--- src/utils/products.data.service.js
+++ src/utils/products.data.service.js
@@ -1,45 +1,40 @@
 (function() {
-  "use strict";
-  angular
-    .module("data")
-    .service("ProductDataService", ProductDataService)
-    .constant("ApiBasePath", "http://127.0.0.1:8080/api/products");
+    "use strict";
+    angular
+        .module("data")
+        .service("ProductDataService", ProductDataService)
+        .constant("ApiBasePath", "http://127.0.0.1:8080/api/products");
 
-  ProductDataService.$inject = ["$http", "ApiBasePath"];
-  function ProductDataService($http, ApiBasePath) {
-    let service = this;
+    ProductDataService.$inject = ["$http", "ApiBasePath"];
 
-    //this method should return a promise which is a result of suing $http service
-    service.getAllProducts = () => {
-      //obtener todas los productos
-      return $http({
-        method: "GET",
-        url: ApiBasePath
-      })
-        .then(result => {
-          return result.data;
-        })
-        .catch(error => console.log(error));
-    };
+    function ProductDataService($http, ApiBasePath) {
+        let service = this;
 
-		service.addProduct = (nombre, cantidad, precioVenta, precioCompra)=> {
-			return $http({
-				method: "POST",
-				url: ApiBasePath,
-				headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-				transformRequest: function(obj) {
-					var str = [];
-					for(var p in obj)
-						str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-					return str.join("&");
-				},
-				data: {nombre: nombre, cantidad: cantidad, precioVenta: precioVenta, precioCompra: precioCompra}
-			})
-				.then(result =>{
-					console.log(result.data);
-					return result.data ? "Se agrego producto con exito" : "Error"
-				})
-				.catch (error => console.log(error))
-		}
-  }
+        //this method should return a promise which is a result of suing $http service
+        service.getAllProducts = () => {
+            //obtener todas los productos
+            return $http({
+                    method: "GET",
+                    url: ApiBasePath
+                })
+                .then(result => {
+                    return result.data;
+                })
+                .catch(error => console.log(error));
+        };
+
+        service.addProduct = (nombre, cantidad, precioVenta, precioCompra) => {
+            return $http({
+                    method: "POST",
+                    url: ApiBasePath,
+                    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+                    data: { nombre: nombre, cantidad: cantidad, precioVenta: precioVenta, precioCompra: precioCompra }
+                })
+                .then(result => {
+                    console.log(result.data);
+                    return result.data ? "Se agrego producto con exito" : "Error"
+                })
+                .catch(error => console.log(error))
+        }
+    }
 })();
